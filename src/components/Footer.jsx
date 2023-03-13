1907b681fdb9f00a0818c72ced551b4656d3ba33
NhatWin <nhat0933@gmail.com>
2023-03-12 22:26:32 -0500

    Work on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ export default function Footer() {
   return (
     <>
       <footer className="mt-8 flex place-content-center justify-center bg-black text-white">
-        <p>© 2023 Nhat Nguyen</p>
+        <a href="https://github.com/NhatWin">
+          <img src="./src/assets/github.png" alt="" className="" id="icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/nhat-nguyen-112349265/">
+          <img src="./src/assets/linkedin.png" alt="" className="" id="icon" />
+        </a>
       </footer>
     </>
   );
