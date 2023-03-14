1673fb9f6130fe56dc74f413da773be86cdcf0c0
NhatWin <nhat0933@gmail.com>
2023-03-14 18:21:03 -0500

    Add fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/demo-react-contact-form/",
   plugins: [react()],
   resolve: {
     alias: {
