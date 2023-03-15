0feabde94bc1e6e7b755fdd152b3bfbfb93566b5
NhatWin <nhat0933@gmail.com>
2023-03-14 21:37:12 -0500

    Remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/demo-react-contact-form/",
   plugins: [react()],
   resolve: {
     alias: {
